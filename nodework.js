14ee51857f4f8c6b3ec1ffff97f31b4f0abf91d6
ddgroleau <ddgroleau@gmail.com>
2021-09-09 09:06:06 -0600

    Continued node.js module.

--- nodework.js
+++ nodework.js
@@ -1,1 +1,16 @@
+let events = require('events');
+
 console.log(process.memoryUsage());
+
+let myEmitter = new events.EventEmitter();
+
+// essentially a node.js event listener
+let callback = (data) => {
+    console.log('user' + data);
+};
+
+// Tells the emitter what event and what to do when that event happens
+myEmitter.on('user', callback);
+
+// performs the emitting, first arg is event and second is callback argument
+myEmitter.emit('user','it worked!');
