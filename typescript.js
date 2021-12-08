aca7b3df75b864e4fe39dd9512c7f18560a36b4c
Dan Groleau <ddgroleau@gmail.com>
2021-12-07 20:49:06 -0700

    Added typescript practice.

--- typescript.js
+++ typescript.js
@@ -1,15 +1,21 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log('Hello!');
 class Typey {
     constructor() {
     }
-    yo(name) {
+    yo(name, func) {
         console.log(`Yo, ${name}!`);
+        return func(name);
     }
     gimmeTypes() {
         return this;
     }
 }
 const typey = new Typey();
-typey.yo('dan');
+typey.yo('dan', () => 'dan');
 console.log(typey.gimmeTypes());
+let tuple = [true, false];
+console.log(tuple);
+let TypeyArray = [typey, 1];
+console.log(TypeyArray);
--- typescript.ts
+++ typescript.ts
@@ -1,12 +1,16 @@
+import { Type } from "typescript";
+
 console.log('Hello!');
+type TypeyFunction = (arg1:string) => string;
 
 class Typey {
     constructor(){
 
     }
 
-    yo(name:string) {
+    yo(name:string,func:TypeyFunction) {
         console.log(`Yo, ${name}!`);
+        return func(name);
     }
 
     gimmeTypes():Typey {
@@ -16,5 +20,13 @@ class Typey {
 
 const typey:Typey = new Typey();
 
-typey.yo('dan');
+typey.yo('dan', ()=>'dan');
+
 console.log(typey.gimmeTypes());
+
+let tuple: [boolean, boolean] = [true, false];
+console.log(tuple);
+
+type MyType = [Typey, number];
+let TypeyArray:MyType = [typey,1];
+console.log(TypeyArray);
